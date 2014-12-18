362df281b936723dfa41ae42e85525f8077c4f05
Yannick <yannick.schutz@gmail.com>
2014-12-18 17:46:43 +0100

    Add methods

--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@ require 'sinatra'
 require 'json'
 
 get %r{/([\d]{3})} do
+  do_the_code
+end
+
+post %r{/([\d]{3})} do
+  do_the_code
+end
+
+patch %r{/([\d]{3})} do
+  do_the_code
+end
+
+put %r{/([\d]{3})} do
+  do_the_code
+end
+
+delete %r{/([\d]{3})} do
+  do_the_code
+end
+
+def do_the_code
   code = params[:captures].first.to_i
   status code
   content_type :json
